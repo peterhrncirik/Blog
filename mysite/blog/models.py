ee7ec4dbfb1a6f62463fb5db5df193074639c233
peto <peto.hrncirik@gmail.com>
2022-10-29 10:36:50 +0200

    views, models, pagination updates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,6 +5,7 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -19,7 +20,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -37,3 +38,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 def post_list(request):
     
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    # Pagination
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
 
     return render(request, 'blog/post/list.xhtml', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
 
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, 
+                            status=Post.Status.PUBLISHED, 
+                            slug=post,
+                            publish__year=year, 
+                            publish__month=month, 
+                            publish__day=day)
 
     return render(request, 'blog/post/detail.xhtml', {'post': post})
 
