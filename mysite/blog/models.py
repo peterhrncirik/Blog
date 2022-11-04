eea10a1fed17e7284385937325d49232783e436b
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-11-04 13:24:54 +0100

    added tagging functionality

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class PublishedManager(models.Manager):
@@ -29,6 +30,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 # Create your views here.
@@ -20,10 +22,13 @@ class PostListView(ListView):
     template_name = 'blog/post/list.xhtml'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     
     post_list = Post.published.all()
-
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -37,7 +42,7 @@ def post_list(request):
         # page num out-of-range
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.xhtml', {'posts': posts})
+    return render(request, 'blog/post/list.xhtml', {'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
 
@@ -53,7 +58,12 @@ def post_detail(request, year, month, day, post):
     # comments form
     form = CommentForm()
 
-    return render(request, 'blog/post/detail.xhtml', {'post': post, 'comments': comments, 'form': form})
+    # similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.xhtml', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 @require_POST
 def post_comment(request, post_id):
