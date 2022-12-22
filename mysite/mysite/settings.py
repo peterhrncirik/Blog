de3cb20484885c2e90e701c4b20a878cb54de5f0
Peter Hrnčírik <42555620+peterhrncirik@users.noreply.github.com>
2022-12-22 08:32:47 +0100

    markdown updates + PostgreSQL migration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -101,6 +101,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': '',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
